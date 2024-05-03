f971b3ada71debfabe41cdae62e79ab3912b1787
Tony Stark <apurificato5680@gmai.com>
2024-05-03 17:34:59 -0400

    app is now fully functional and working, it outputs a new svg file upon completion of the inquirer prompt in the terminal, and application passes all of the tests for jest.

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-// TODO: Include packages needed for this application
-const fs = require('fs');
+const fs = require('fs')
 const inquirer = require('inquirer');
+const shapes = require('./lib/shapes');
 
-// Generate SVG File by shape // case breaks depending on shape chosen
+// Function to generate SVG Data Based on User Input and Gives a Choice of Different Shapes Data That Will Be Outputted in the Newly Created SVG File.
 function generateSVG(data) {
     let shapeSVG;
     switch (data.shape) {
         case 'Circle':
-            shapeSVG = new Circle(data.title, data.textColor, data.shapeColor).renderSVG();
+            shapeSVG = new shapes.Circle(data.title, data.titleColor, data.shapeColor).renderSVG();
             break;
         case 'Triangle':
-            shapeSVG = new Triangle(data.title, data.textColor, data.shapeColor).renderSVG();
+            shapeSVG = new shapes.Triangle(data.title, data.titleColor, data.shapeColor).renderSVG();
             break;
         case 'Square':
-            shapeSVG = new Square(data.title, data.textColor, data.shapeColor).renderSVG();
+            shapeSVG = new shapes.Square(data.title, data.titleColor, data.shapeColor).renderSVG();
             break;
         default:
             shapeSVG = '';
             break;
     }
-};
+    return shapeSVG; // Return the generated SVG
+}
 
-// Prompt for an array of questions to take SVG Parameters
-const questions = inquirer.prompt ([
+// Prompt For an Array of Questions That Takes in the SVG Parameters
+const questions = [
     {
         name: 'title',
         message: 'Choose a short title for your logo (3 character Max).',
         prefix: '\n',
         validate: function(input) {
             if (input.length > 3) {
-                return 'Title must be 3 characters max'
+                return 'Title must be 3 characters max';
             }
             return true;
         }
     },
-
     {
         type: 'list',
         name: 'titleColor',
@@ -42,49 +42,38 @@ const questions = inquirer.prompt ([
         choices: ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black'],
         prefix: '\n'
     },
-
     {
         type: 'list',
         name: 'shape',
         message: 'What shape do you want to choose',
         choices: ['Square', 'Circle', 'Triangle'],
     },
-
-    {   
+    {
         type: 'list',
         name: 'shapeColor',
         message: 'What color do you want the shape to be?',
         choices: ['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black'],
         prefix: '\n'
     }
-]).then(answers => {
-    // Generate SVG code
-    const svgCode = `
-    <svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-    ${shapeSVG}
-    </svg>`;
+];
 
-    // Write SVG to file
-    fs.writeFileSync('logo.svg', svgCode);
+// Inquirer Prompt - Asks Questions and Takes User Input for Building File. Then Generates SVG File Output and Writes the File Using FS Dependency (Native to Node).
+inquirer
+    .prompt(questions)
+    .then(answers => {
+        console.log('Answers:', answers);
+        const shapeSVG = generateSVG(answers);
 
-    console.log('Generated logo.svg file successfully!');
-}).catch(error => {
-    console.error('Error:', error);
-});
+        const svgCode = `
+        <svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        ${shapeSVG}
+        </svg>`;
 
-// Function that triggers prompt and asks questions for building markdown data
-// function ask() {
-//     inquirer.prompt(questions)
-//     .then(answers => {
-//         console.log('Answer', answers)
-//         const newLogoFile = generateSVG(answers)
+        // Writes SVG Code to a Newly Created File
+        fs.writeFileSync('./examples/logo.svg', svgCode);
 
-//         writeToFile('logo.svg', newLogoFile)
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-// }
-
-// Function call to initialize app
-// ask();
+        console.log('Generated logo.svg file successfully!');
+    })
+    .catch((err) => {
+        console.error('Error:', err);
+    });
